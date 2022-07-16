336be83d516ccaaef57a5ae97207f17cea9b1625
edbarstad <ed@barstad.com>
2022-07-15 21:07:53 -0600

    Change module name

--- mix.exs
+++ mix.exs
@@ -1,8 +1,8 @@
-defmodule NervesSystemRpi3.MixProject do
+defmodule RockPro64.MixProject do
   use Mix.Project
 
-  @github_organization "nerves-project"
-  @app :nerves_system_rpi3
+  @github_organization "edbarstad"
+  @app :rockpro64
   @source_url "https://github.com/#{@github_organization}/#{@app}"
   @version Path.join(__DIR__, "VERSION")
            |> File.read!()
